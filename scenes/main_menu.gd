0796255cbf002f7581dcea9e0bb0b2e5484837c5
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2023-06-02 16:46:25 +0200

    Basic intro text (currently wip) and PC name changer

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -3,6 +3,7 @@ extends CenterContainer
 
 func _on_NewGame_pressed():
 	Dialogic.reset_saves()
+	Dialogic.set_variable("pc_name", $"%PCName".text)
 	get_tree().change_scene("res://scenes/Game.tscn")
 
 
