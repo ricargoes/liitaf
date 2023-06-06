823f2c08cb0b78fbdbaaadf991ccf39723bbff12
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2023-06-06 23:37:04 +0200

    New levels for music

--- scenes/music.gd
+++ scenes/music.gd
@@ -4,8 +4,11 @@ var current_track = "Base"
 
 func transition_to(new_track):
 	get_node(current_track).volume_db = -80
-	get_node(new_track).volume_db = 0
+	get_node(new_track).volume_db = -20
 	current_track = new_track
 	
 func distorsion_level(new_level):
-	$Distorsion.volume_db = -30 + new_level*4
+	var fl = -80
+	if new_level > 0:
+		fl = -15 + (-20/new_level)
+	$Distorsion.volume_db = fl
